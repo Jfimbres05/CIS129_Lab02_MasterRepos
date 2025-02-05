1bc9af64f39628b5ca68f26bebd1cbf136bc428f
Jfimbres05 <jfimbres22@mail.pima.edu>
2025-02-04 20:31:20 -0700

    Updated cis129_lab02_shortStory.py. Prints a short story about Bob and programming.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,13 @@
-print('Hello, World!')
+# Author: Julian Fimbres
+# My first Python lab in my CIS129 course.
+# Prints a simple short story about Bob.
+
+print("There once was a Python Pogrammer named Bob.")
+
+print("One day, Bob was writing his code on Github. He was developing the backend of a marketing website.")
+
+print("Bob had made some BIG changes to the code, and committed all his changes to the main branch without testing.")
+
+print("Turns out, there was a major bug within the new changes. Bob pushed his changes therefore the entire Main Branch was affected, since Bob didn't commit his changes in a new branch, he doesn't know where to find the effected area")
+
+print("Eventually, the bug is fixed by helpful contributors, and the project is saved! Bob will never commit changes on the Main Branch again.")
